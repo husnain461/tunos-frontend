f9520b66972a4871036c4e7d58a0fae5b2beb390
Kunj-Choksi <kunj07soni@gmail.com>
2021-11-13 18:51:37 -0500

    Fixed bug for heroku deploment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static("./dist/tunos-frontend"));
 
 app.get("/*", (req, res) => {
-    res.sendFile("index.html", {root: "dist/angular-heroku"})
+    res.sendFile("index.html", {root: "dist/Tunos"})
 })
 
 app.listen(process.env.PORT || 8080);
