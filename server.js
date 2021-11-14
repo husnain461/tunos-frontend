0e1955aef4a8b9dd4d6dd32cbfb9ab8ce0f95b0d
Kunj-Choksi <kunj07soni@gmail.com>
2021-11-13 19:18:41 -0500

    Reverted index.html and Fixed path server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static("./dist/tunos-frontend"));
+app.use(express.static("./dist/Tunos"));
 
 app.get("/*", (req, res) => {
     res.sendFile("index.html", {root: "dist/Tunos"})
